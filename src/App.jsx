46bb34bded8906d96afd9a688ad3936a3b53bf45
Aditya Chopra <nytc120605@gmail.com>
2025-04-30 16:14:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Todo from './components/ToDo'
+import ToDo from './components/ToDo'
 
 function App() {
   return (
     <div className='bg-stone-900 grid py-4 min-h-screen'>
-      <Todo/>
+      <ToDo/>
     </div>
   )
 }
