cd2036520ab2602ea9792f9c92a8932d863301b6
Aditya Chopra <nytc120605@gmail.com>
2025-04-30 16:14:02 +0530

    Update and rename Todo.jsx to ToDo.jsx

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -3,7 +3,7 @@ import calender from '../assets/calender.png'
 import PlusSignBtn from '../assets/PlusSignBtn.png'
 import Todoitems from './Todoitems'
 
-const Todo = () => {
+const ToDo = () => {
 
   const [todoList, setTodoList] = useState(localStorage.getItem("todos")?JSON.parse(localStorage.getItem("todos")) : []);
 
@@ -78,4 +78,4 @@ useEffect(() => {
   )
 }
 
-export default Todo
+export default ToDo
